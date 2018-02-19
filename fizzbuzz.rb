74600ae7a4d6dd723c2d3c30fd134514060b8909
Austin Wolfe <wolfe_austin@yahoo.com>
2018-02-19 22:40:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,5 @@
 def fizzbuzz(number)
   if number % 3 == 0 
-    if number % 5 == 0 
-      "FizzBuzz"
-    else 
-      "Fizz"
-    end 
-  elsif number % 5==0
-    "Buzz"
+    "Fizz"
   end 
 end 
